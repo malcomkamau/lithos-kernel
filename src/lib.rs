5c5fd8a7c2478551e6c3e421b83c183d398a1642
malcomkamau <malcomkamau504@gmail.com>
2026-02-15 02:41:55 +0300

    feat: Implement basic memory management with page table initialization and a boot info frame allocator.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod memory;
 
 pub fn init() {
     gdt::init();
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,40 @@
 
 use core::panic::PanicInfo;
 use lithos::println;
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Lithos Kernel OS booting...");
+use bootloader::{BootInfo, entry_point};
 
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use lithos::memory;
+    use x86_64::{VirtAddr, structures::paging::Translate};
+
+    println!("Lithos Kernel OS booting...");
     lithos::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let _frame_allocator = unsafe {
+        memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    // translate some addresses
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     println!("It did not crash!");
 
